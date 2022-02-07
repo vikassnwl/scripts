c9ca7b2f5861801bc3583ebc3e9cd626391f9efa
Vikas Sanwal <vikassnwl@gmail.com>
2022-02-07 09:59:53 +0530

    update comparator.py

--- python/comparator.py
+++ python/comparator.py
@@ -2,15 +2,18 @@ from functools import cmp_to_key
 
 lst_of_tup = [(3, 1), (1, 3), (2, 2)]
 
-# sort based on first item of tuple in ascending order
-def srt_asc(tup1, tup2):
-    return tup1[0] - tup2[0]
+# # sort based on first item of tuple in ascending order
+# def srt_asc(tup1, tup2):
+#     return tup1[0] - tup2[0]
 
-# sort based on first item of tuple in descending order
-def srt_desc(tup1, tup2):
-    return -(tup1[0] - tup2[0])
+# # sort based on first item of tuple in descending order
+# def srt_desc(tup1, tup2):
+#     return -(tup1[0] - tup2[0])
 
 
-lst_of_tup.sort(key=cmp_to_key(srt_desc))
+# lst_of_tup.sort(key=cmp_to_key(srt_desc))
 
+# print(lst_of_tup)
+
+lst_of_tup.sort(key=lambda x: x[0], reverse=True)
 print(lst_of_tup)
